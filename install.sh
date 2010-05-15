fd054ad77c6531d5b671ae9612d914f3e64fe77d
David Slabý <53720+blaf@users.noreply.github.com>
2010-05-15 00:27:50 +0000

    tinyxml added to install script, subdirectories created in ~/.ditchers if necessary

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo apt-get install g++ make libsdl1.2-dev libsdl-image1.2-dev libsdl-net1.2-dev libsdl-gfx1.2-dev libpng12-dev libguichan-dev libboost-filesystem-dev liblua5.1-0-dev
+sudo apt-get install g++ make libsdl1.2-dev libsdl-image1.2-dev libsdl-net1.2-dev libsdl-gfx1.2-dev libpng12-dev libguichan-dev libboost-filesystem-dev liblua5.1-0-dev libtinyxml-dev
 sudo make install
