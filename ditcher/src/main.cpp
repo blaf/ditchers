b4d93f1b481931955ffb4d2e4a21a225bb61ea1a
David Slabý <53720+blaf@users.noreply.github.com>
2010-05-17 08:33:21 +0000

    GPL information added

--- ditcher/src/main.cpp
+++ ditcher/src/main.cpp
@@ -3,6 +3,21 @@
 Ditcher is the main part of the Ditchers application. It manages the whole gameplay and GUI.
 
 Main classes are UserFace and GamePlay that care for GUI and gameplay and other important classes are Settings, Gfx and Network.
+
+Copyright (C) 2010  David Slaby
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <iostream>
